4620df451823693cee312a7ce56eb5c8ab41020d
Karl Blomster <kalle@agigen.se>
2011-05-29 19:05:08 +0200

    sms vote frontend api

--- ucxi-utilities/ucsmsvote/main.py
+++ ucxi-utilities/ucsmsvote/main.py
@@ -1,31 +1,25 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext import db
+from django.utils import simplejson as json
+import random
+import time
+import datetime
+import logging
 
 
-class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+class Poll(db.Model):
+    name        = db.StringProperty(required=True)
+    createdAt   = db.DateTimeProperty()
 
 
-class AdminHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('admin lol')
+class Option(db.Model):
+    oid         = db.StringProperty(required=True)
+    name        = db.StringProperty(required=True)
+    createdAt   = db.DateTimeProperty()
+
 
 
 class VoteHandler(webapp.RequestHandler):
@@ -33,10 +27,100 @@ class VoteHandler(webapp.RequestHandler):
         self.response.out.write('lax')
 
 
+class PollHandler(webapp.RequestHandler):
+    def get(self):
+        out = []
+        for p in Poll.all():
+            out.append({'shortname': p.key().name(), 'name': p.name})
+
+        self.response.out.write(json.dumps(out))
+
+
+    def post(self):
+        name        = self.request.get('name')
+        shortname   = self.request.get('shortname')
+
+        if name == '' or shortname == '':
+            self.response.out.write(json.dumps({'status': "invalid name or shortname"}))
+            self.response.set_status(404)
+            return
+
+        poll = db.get(db.Key.from_path('Poll', shortname))
+        if poll != None:
+            self.response.out.write(json.dumps({'status': 'shortname already exists'}))
+            self.response.set_status(403)
+            return
+
+        poll = Poll(key_name=shortname, name=name, createdAt=datetime.datetime.now())
+        poll.put()
+
+        self.response.out.write(json.dumps({'status': 'ok'}))
+
+
+    def delete(self, shortname):
+        poll = db.get(db.Key.from_path('Poll', shortname))
+        if poll == None:
+            self.response.set_status(404)
+            return
+
+        poll.delete()
+        self.response.out.write(json.dumps({'status': 'ok'}))
+
+
+
+class OptionHandler(webapp.RequestHandler):
+    def get(self):
+        shortname   = self.request.get('shortname')
+        polls       = Poll.all()
+        if shortname:
+            polls.ancestor(db.Key.from_path('Poll', shortname))
+
+        out = {}
+        for p in polls:
+            options = {}
+            for o in Option.all().ancestor(p.key()):
+                options[o.oid] = {'name': o.name}
+            out[p.key().name()]
+
+        self.response.out.write(json.dumps(out))
+
+
+    def post(self):
+        shortname   = self.request.get('shortname')
+        oid         = self.request.get('id')
+        name        = self.request.get('name')
+
+        poll = db.get(db.Key.from_path('Poll', shortname))
+        if not poll:
+            self.response.out.write(json.dumps({'status': 'no such poll'}))
+            self.response.set_status(404)
+            return
+
+        option = Option(oid=oid, name=name, parent=poll.key())
+        option.put()
+
+        self.response.out.write(json.dumps({'status': 'ok'}))
+
+
+    def delete(self, shortname, oid):
+        o = Option.all().ancestor(db.Key.from_path('Poll', shortname)).filter('oid =', oid).get()
+        if not o:
+            self.response.out.write(json.dumps({'status': 'no such option'}))
+            self.response.set_status(404)
+            return
+
+        o.delete()
+        self.response.out.write(json.dumps({'status': 'ok'}))
+
+
+
+
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler),
-                                          ('/vote', VoteHandler),
-                                          ('/admin', AdminHandler)]
+    application = webapp.WSGIApplication([('/vote', VoteHandler),
+                                          ('/poll', PollHandler),
+                                          ('/poll/(\w+)', PollHandler),
+                                          ('/option', OptionHandler),
+                                          ('/option/(\w+)/([\w\d]+)', OptionHandler)],
                                          debug=True)
     util.run_wsgi_app(application)
 
